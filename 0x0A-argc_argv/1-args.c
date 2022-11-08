a1c33d047ab2fc915937f1cdb8c768df6eca4ac1
KibetBii <franklinebee@gmail.com>
2022-11-08 07:20:29 -0800

    write

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - prints the name of the program
+  * @argc: counts arguements
+  * @argv: arguments
+  * Return: Always 0(success)
+  */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
